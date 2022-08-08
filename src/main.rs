72e97bee09fcda13d5010990c2742da99b3b301b
DADpea <40965707+DADpea@users.noreply.github.com>
2022-08-07 23:24:10 -0600

    Adding context

--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,5 @@ mod compiler;
 
 fn main() {
     println!("Hello, world!");
-    tokenizer::tokenizer("fn proc SAVE if yay :: : :");
+    tokenizer::tokenizer("fn proc SAVE if yay :: : :"); // EOF here at ':', add better EOF messages in the future 
 }
