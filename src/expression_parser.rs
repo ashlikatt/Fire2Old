8b95bc31095a3ab2515669fff07abdd80ae38f71
ashley <sqrtseven7@gmail.com>
2022-08-19 21:05:45 -0500

    Expression parser nearly done~!

--- src/expression_parser.rs
+++ src/expression_parser.rs
@@ -16,7 +16,7 @@
 
 use std::collections::VecDeque;
 
-use crate::tokenizer::Token;
+use crate::{tokenizer::Token, compiler};
 
 #[derive(Debug)]
 pub enum Value {
@@ -25,6 +25,16 @@ pub enum Value {
         name: Method,
         parameters: Option<Vec<Value>>
     },
+    GetProperty {
+        on: Box<Value>,
+        name: String
+    },
+    Greater(Box<Value>),
+    Less(Box<Value>),
+    GreaterEqual(Box<Value>),
+    LessEqual(Box<Value>),
+    Equal(Box<Value>),
+    NotEqual(Box<Value>),
     String(String),
     Num(f32),
     Int(i64),
@@ -40,163 +50,168 @@ pub enum Method {
     Absolute(Vec<String>),     // Runs the specific method at that location
 }
 
-fn parse_expression(queue: &mut VecDeque<Token>) -> Value {
-    let first = parse_and(queue);
-    if let Some(Token::BoolOr) = queue.front() { // If there is more to read, and the next symbol is an OR...
-        queue.pop_front();                              // Remove the OR
-        let second = parse_expression(queue);         // Parse another expression after the OR
-        Value::MethodCall { 
-            on: Box::new(first), 
-            name: Method::Derive(vec![String::from("std"),String::from("operators"),String::from("Or"),String::from("or")]), 
-            parameters: Some(vec![second]) 
+fn parse_expression(queue: &mut VecDeque<Token>) -> Result<Value, compiler::CompileError> {
+    let first = parse_value(queue)?;
+    if let Some(t) = queue.front() { 
+        match t {
+            Token::OpenBrace => {
+                let second = parse_expression(queue)?;
+                if let Some(Token::CloseBrace) = queue.front() {
+                    queue.pop_front();
+                    Ok(Value::MethodCall { 
+                        on: Box::new(first), 
+                        name: Method::Derive(vec![String::from("std"),String::from("operators"),String::from("Accessible"),String::from("access")]), 
+                        parameters: Some(vec![second]) 
+                    })
+                } else {
+                    Err(compiler::CompileError { error_type: compiler::ErrorType::InvalidTokenError, location: 0 }) // 0 for now
+                }
+            }
+            Token::OpenParen => { // TODO THIS
+                todo!()
+            }
+            Token::Dot => {
+                queue.pop_front();
+                let second = parse_expression(queue)?;
+                if let Value::String(s) = second {
+                    Ok(Value::GetProperty { 
+                        on: Box::new(first), 
+                        name: s,
+                    })
+                } else {
+                    Err(compiler::CompileError { error_type: compiler::ErrorType::InvalidTokenError, location: 0 }) // 0 for now
+                }
+            }
+            Token::Multiply => {
+                queue.pop_front();
+                let second = parse_expression(queue)?;
+                Ok(Value::MethodCall { 
+                    on: Box::new(first), 
+                    name: Method::Derive(vec![String::from("std"),String::from("operators"),String::from("Multiply"),String::from("multiply")]), 
+                    parameters: Some(vec![second]) 
+                })
+            }
+            Token::Divide => {
+                queue.pop_front();
+                let second = parse_expression(queue)?;
+                Ok(Value::MethodCall { 
+                    on: Box::new(first), 
+                    name: Method::Derive(vec![String::from("std"),String::from("operators"),String::from("Divide"),String::from("divide")]), 
+                    parameters: Some(vec![second]) 
+                })
+            }
+            Token::Modulo => {
+                queue.pop_front();
+                let second = parse_expression(queue)?;
+                Ok(Value::MethodCall { 
+                    on: Box::new(first), 
+                    name: Method::Derive(vec![String::from("std"),String::from("operators"),String::from("Modulo"),String::from("modulo")]), 
+                    parameters: Some(vec![second]) 
+                })
+            }
+            Token::Sum => {
+                queue.pop_front();
+                let second = parse_expression(queue)?;
+                Ok(Value::MethodCall { 
+                    on: Box::new(first), 
+                    name: Method::Derive(vec![String::from("std"),String::from("operators"),String::from("Add"),String::from("add")]), 
+                    parameters: Some(vec![second]) 
+                })
+            }
+            Token::Minus => {
+                queue.pop_front();
+                let second = parse_expression(queue)?;
+                Ok(Value::MethodCall { 
+                    on: Box::new(first), 
+                    name: Method::Derive(vec![String::from("std"),String::from("operators"),String::from("Subtract"),String::from("subtract")]), 
+                    parameters: Some(vec![second]) 
+                })
+            }
+            Token::Greater => {
+                queue.pop_front();
+                let second = parse_expression(queue)?;
+                Ok(Value::Greater(Box::new(Value::MethodCall { 
+                    on: Box::new(first), 
+                    name: Method::Derive(vec![String::from("std"),String::from("operators"),String::from("Comparable"),String::from("compare")]), 
+                    parameters: Some(vec![second]) 
+                })))
+            }
+            Token::GreaterEqual => {
+                queue.pop_front();
+                let second = parse_expression(queue)?;
+                Ok(Value::GreaterEqual(Box::new(Value::MethodCall { 
+                    on: Box::new(first), 
+                    name: Method::Derive(vec![String::from("std"),String::from("operators"),String::from("Comparable"),String::from("compare")]), 
+                    parameters: Some(vec![second]) 
+                })))
+            }
+            Token::Less => {
+                queue.pop_front();
+                let second = parse_expression(queue)?;
+                Ok(Value::Less(Box::new(Value::MethodCall { 
+                    on: Box::new(first), 
+                    name: Method::Derive(vec![String::from("std"),String::from("operators"),String::from("Comparable"),String::from("compare")]), 
+                    parameters: Some(vec![second]) 
+                })))
+            }
+            Token::LessEqual => {
+                queue.pop_front();
+                let second = parse_expression(queue)?;
+                Ok(Value::LessEqual(Box::new(Value::MethodCall { 
+                    on: Box::new(first), 
+                    name: Method::Derive(vec![String::from("std"),String::from("operators"),String::from("Comparable"),String::from("compare")]), 
+                    parameters: Some(vec![second]) 
+                })))
+            }
+            Token::Equals => {
+                queue.pop_front();
+                let second = parse_expression(queue)?;
+                Ok(Value::Equal(Box::new(Value::MethodCall { 
+                    on: Box::new(first), 
+                    name: Method::Derive(vec![String::from("std"),String::from("operators"),String::from("Comparable"),String::from("compare")]), 
+                    parameters: Some(vec![second]) 
+                })))
+            }
+            Token::NotEquals => {
+                queue.pop_front();
+                let second = parse_expression(queue)?;    
+                Ok(Value::NotEqual(Box::new(Value::MethodCall { 
+                    on: Box::new(first), 
+                    name: Method::Derive(vec![String::from("std"),String::from("operators"),String::from("Comparable"),String::from("compare")]), 
+                    parameters: Some(vec![second]) 
+                })))
+            }
+            Token::BoolAnd => {
+                queue.pop_front();
+                let second = parse_expression(queue)?; 
+                Ok(Value::MethodCall { 
+                    on: Box::new(first), 
+                    name: Method::Derive(vec![String::from("std"),String::from("operators"),String::from("And"),String::from("and")]), 
+                    parameters: Some(vec![second]) 
+                })
+            }
+            Token::BoolOr => {
+                queue.pop_front();
+                let second = parse_expression(queue)?;
+                Ok(Value::MethodCall { 
+                    on: Box::new(first), 
+                    name: Method::Derive(vec![String::from("std"),String::from("operators"),String::from("Or"),String::from("or")]), 
+                    parameters: Some(vec![second]) 
+                })
+            }
+            _ => Ok(first) // Nothing left :3
         }
     } else {
-        first
+        Ok(first)
     }
 }
 
-fn parse_and(queue: &mut VecDeque<Token>) -> Value {
-    let first = parse_equals(queue);
-    if let Some(Token::BoolAnd) = queue.front() { 
-        queue.pop_front();                            
-        let second = parse_expression(queue);    
-        Value::MethodCall { 
-            on: Box::new(first), 
-            name: Method::Derive(vec![String::from("std"),String::from("operators"),String::from("And"),String::from("and")]), 
-            parameters: Some(vec![second]) 
-        }
-    } else {
-        first
-    }
-}
-
-fn parse_equals(queue: &mut VecDeque<Token>) -> Value {
-    let first = parse_comparison(queue);
-    if let Some(Token::Equals) = queue.front() {
-        queue.pop_front();                             
-        let second = parse_expression(queue);      
-        Value::MethodCall { 
-            on: Box::new(first), 
-            name: Method::Derive(vec![String::from("std"),String::from("operators"),String::from("Comparable"),String::from("isEqual")]), 
-            parameters: Some(vec![second]) 
-        }
-    } else if let Some(Token::NotEquals) = queue.front() {
-        queue.pop_front();                             
-        let second = parse_expression(queue);      
-        Value::MethodCall { 
-            on: Box::new(first), 
-            name: Method::Derive(vec![String::from("std"),String::from("operators"),String::from("Comparable"),String::from("isNotEqual")]), 
-            parameters: Some(vec![second]) 
-        }
-    } else {
-        first
-    }
-}
-
-fn parse_comparison(queue: &mut VecDeque<Token>) -> Value {
-    let first = parse_sum(queue);
-    if let Some(Token::Greater) = queue.front() { 
-        queue.pop_front();                            
-        let second = parse_expression(queue);     
-        Value::MethodCall { 
-            on: Box::new(first), 
-            name: Method::Derive(vec![String::from("std"),String::from("operators"),String::from("Comparable"),String::from("isGreater")]), 
-            parameters: Some(vec![second]) 
-        }
-    } else if let Some(Token::Less) = queue.front() {
-        queue.pop_front();                             
-        let second = parse_expression(queue);    
-        Value::MethodCall { 
-            on: Box::new(first), 
-            name: Method::Derive(vec![String::from("std"),String::from("operators"),String::from("Comparable"),String::from("isLess")]), 
-            parameters: Some(vec![second]) 
-        }
-    } else if let Some(Token::GreaterEqual) = queue.front() {
-        queue.pop_front();                             
-        let second = parse_expression(queue);      
-        Value::MethodCall { 
-            on: Box::new(first), 
-            name: Method::Derive(vec![String::from("std"),String::from("operators"),String::from("Comparable"),String::from("isGreaterEq")]), 
-            parameters: Some(vec![second]) 
-        }
-    } else if let Some(Token::LessEqual) = queue.front() {
-        queue.pop_front();                             
-        let second = parse_expression(queue);     
-        Value::MethodCall { 
-            on: Box::new(first), 
-            name: Method::Derive(vec![String::from("std"),String::from("operators"),String::from("Comparable"),String::from("isLessEq")]), 
-            parameters: Some(vec![second]) 
-        }
-    } else {
-        first
-    }
-}
-
-fn parse_sum(queue: &mut VecDeque<Token>) -> Value {
-    let first = parse_term(queue);
-    if let Some(Token::Sum) = queue.front() { 
-        queue.pop_front();                             
-        let second = parse_expression(queue);     
-        Value::MethodCall { 
-            on: Box::new(first), 
-            name: Method::Derive(vec![String::from("std"),String::from("operators"),String::from("Add"),String::from("add")]), 
-            parameters: Some(vec![second]) 
-        }
-    } else if let Some(Token::Minus) = queue.front() {
-        queue.pop_front();                             
-        let second = parse_expression(queue);     
-        Value::MethodCall { 
-            on: Box::new(first), 
-            name: Method::Derive(vec![String::from("std"),String::from("operators"),String::from("Subtract"),String::from("subtract")]), 
-            parameters: Some(vec![second]) 
-        }
-    } else {
-        first
-    }
-}
-
-fn parse_term(queue: &mut VecDeque<Token>) -> Value {
-    let first = parse_accessor(queue);
-    if let Some(Token::Multiply) = queue.front() { 
-        queue.pop_front();                             
-        let second = parse_expression(queue);      
-        Value::MethodCall { 
-            on: Box::new(first), 
-            name: Method::Derive(vec![String::from("std"),String::from("operators"),String::from("Multiply"),String::from("multiply")]), 
-            parameters: Some(vec![second]) 
-        }
-    } else if let Some(Token::Divide) = queue.front() {
-        queue.pop_front();                             
-        let second = parse_expression(queue);    
-        Value::MethodCall { 
-            on: Box::new(first), 
-            name: Method::Derive(vec![String::from("std"),String::from("operators"),String::from("Divide"),String::from("divide")]), 
-            parameters: Some(vec![second]) 
-        }
-    } else if let Some(Token::Modulo) = queue.front() {
-        queue.pop_front();                             
-        let second = parse_expression(queue);     
-        Value::MethodCall { 
-            on: Box::new(first), 
-            name: Method::Derive(vec![String::from("std"),String::from("operators"),String::from("Modulo"),String::from("modulo")]), 
-            parameters: Some(vec![second]) 
-        }
-    } else {
-        first
-    }
-}
-
-fn parse_accessor(queue: &mut VecDeque<Token>) -> Value {
-    let first = parse_value(queue);
-    // Parse [] here
-    // Parse <val>.<ident> here, or <val>.<ident>(<params>)    
-    
-    unimplemented!()
-}
-
-
-fn parse_value(queue: &mut VecDeque<Token>) -> Value {
+fn parse_value(queue: &mut VecDeque<Token>) -> Result<Value, compiler::CompileError> {
     let token = queue.pop_front();
     // Parse the token into a raw Value
-    unimplemented!()
+    todo!()
+}
+
+fn parse_param_list(queue: &mut VecDeque<Token>) -> Result<Value, compiler::CompileError> {
+    todo!()
 }
